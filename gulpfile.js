7c4580287f5ee05425e25c9812e1b850f0821a0c
LinusVillain <ksalamatina95@gmail.com>
2021-09-12 19:28:42 +0300

    4. Настроена сборка через Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
-const sass = require('gulp-sass')(require('sass'));
+const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const mincss = require("gulp-csso");
+const rename = require("gulp-rename");
+const del = require("del");
+const minjs = require("gulp-terser");
+const minHtml = require("gulp-htmlmin");
 
 // Styles
 
@@ -16,10 +21,13 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(mincss())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
-}
+};
 
 exports.styles = styles;
 
@@ -28,24 +36,75 @@ exports.styles = styles;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "build"
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 exports.server = server;
+
+//  Html
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("build"))
+    .pipe(minHtml({ collapseWhitespace: true }))
+    .pipe(rename(function (path) {
+      path.basename += ".min";
+    }))
+    .pipe(gulp.dest("build"))
+    .pipe(sync.stream());
+};
+
+exports.html = html;
+
+//  Clean
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
 
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
-}
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/js/**/*.js", gulp.series(scripts));
+  gulp.watch("source/*.html", gulp.series(html));
+};
+
+//Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/**/*.js")
+    .pipe(gulp.dest("build/js"))
+    .pipe(minjs())
+    .pipe(rename("index.min.js"))
+    .pipe(gulp.dest("build/js"));
+};
 
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+const build = gulp.series(
+  clean,
+  styles,
+  scripts,
+  html
+);
+
+exports.build = build;
+
+const start = gulp.series(
+  build,
+  server,
+  watcher
+);
+
+exports.start = start;
